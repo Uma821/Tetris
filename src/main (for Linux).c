3d48619e7bd2e88a70572811052f654abf5cd24a
uma821 <uma.ohmoto@gmail.com>
2019-08-09 16:18:45 +0900

    :wrench: 不具合を修正しました！
    下ボタンを押しても下がらないなどの不具合を修正しました

--- src/main (for Linux).c
+++ src/main (for Linux).c
@@ -1,4 +1,4 @@
-//テトリスver1.0 for Linux
+//テトリスver1.1 for Linux
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -34,7 +34,7 @@ int holdnow = 0;
 int linekazu = 0;
 
 int main(void) {
-    int rakka = 0;
+    int gcon = 1;
     time_t start_time = clock();
     srand((unsigned)time(NULL));
     nextpush(1);
@@ -43,22 +43,22 @@ int main(void) {
     mainclear();
     dainyu();
     hyoji();
-    while(!game()) {
+    while(gcon) {
         if(kbhit())
             idou();
         if((double)(clock() - start_time) / CLOCKS_PER_SEC > dtime){
             start_time = clock();
-            rakka = hyoji();
             x++;
-            if(rakka == 1){
+            if(hyoji()){
+                x++;
                 teityaku();
+                gcon = game();
                 x = 0;
                 y = 3;
-                rakka = 0;
                 dainyu();
                 linekeshi();
-                hyoji();
                 x++;
+                hyoji();
             }
         }
     }
@@ -67,11 +67,10 @@ int main(void) {
 }
 
 int game(void){
-    for(int i=1; i<12; i++){
-        if(field[0][i] == 1)
-            return 1;
-    }
-    return 0;
+    for(int i=1; i<12; i++)
+        if(field[0][i])
+            return 0;
+    return 1;
 }
 
 int linekeshi(void){
@@ -79,9 +78,9 @@ int linekeshi(void){
     do{
         linekazu = 0;
         int i, j, n, bou[2];
-        for(i=19; i>=0; i--){
+        for(i=19; i>=0; i--)
             for(j=1; j<11; j++){
-                if(field[i][j] == 0){
+                if(!field[i][j]){
                     if(linekazu > 0){
                         bou[1] = i;
                         goto end;
@@ -89,21 +88,18 @@ int linekeshi(void){
                     break;
                 }
                 if(j == 10){
-                    if(linekazu == 0){
-                    bou[0] = i;
-                    }
+                    if(!linekazu)
+                        bou[0] = i;
                     linekazu++;
                 }
             }
-        }
         end:
-            if(linekazu > 0){
-                for(j=bou[0]; j>=bou[0]-bou[1]; j--){
+            if(linekazu > 0)
+                for(j=bou[0]; j>=bou[0]-bou[1]; j--)
                     for(n=1; n<11; n++)
                         field[j][n] = field[j-(bou[0]-bou[1])][n];
-                }
-            }
-        }while(linekazu != 0);
+
+    }while(linekazu);
     return 0;
 }
 
@@ -112,31 +108,33 @@ int idou(void){
     if(h == 'l'){
         right:
         y++;
-        while(hyoji()){
+        if(hyoji()){
             y--;
+            hyoji();
         }
     }
     else if(h == 'k'){
         left:
         y--;
-        while(hyoji()){
+        if(hyoji()){
             y++;
+            hyoji();
         }
     }
     else if(h == 'm'){
         down:
         x++;
-        if(x > 19)
-            x = 20;
-        while(hyoji()){
+        if(hyoji()){
             x--;
+            hyoji();
         }
     }
     else if(h == 'j'){
         turn:
         turnblok();
-        while(hyoji()){
+        if(hyoji()){
             gturn();
+            hyoji();
         }
     }
     else if(h == 'h'){
@@ -157,86 +155,71 @@ int idou(void){
             goto turn;
     }
     //矢印キー入力②
-    else if(h == '\x1b'){
+    else if(h == '\x1b' && getchar() == '\x5b'){
         h = getchar();
-        if(h == '\x5b'){
-            h = getchar();
-            if(h == '\x43')
-                goto right;
-            else if(h == '\x44')
-                goto left;
-            else if(h == '\x42')
-                goto down;
-            else if(h == '\x41')
-               goto turn;
-        }
+        if(h == '\x43')
+            goto right;
+        else if(h == '\x44')
+            goto left;
+        else if(h == '\x42')
+            goto down;
+        else if(h == '\x41')
+           goto turn;
     }
     return 0;
 }
 
 int holdin(void){
+    int i, j;
     //holdに入れる
-    if(holdnow == 0){
-        for(int j=0;j<4;j++){
-            for(int i=0;i<4;i++)
+    if(!holdnow){
+        for(j=0; j<4; j++)
+            for(i=0;i<4;i++)
                 hold[j][i] = next[j][i][0];
-        }
         holdnow = 1;
         dainyu();
     }
-    else if(holdnow == 1){
-        for(int j=0;j<4;j++){
-            for(int i=0;i<4;i++)
+    else{
+        for(j=0; j<4; j++)
+            for(i=0; i<4; i++)
                 holdtemp[j][i] = next[j][i][0];
-        }
-        for(int j=0;j<4;j++){
-            for(int i=0;i<4;i++)
+        for(j=0; j<4; j++)
+            for(i=0; i<4; i++)
                 next[j][i][0] = block[j][i] = hold[j][i];
-        }
-        for(int j=0;j<4;j++){
-            for(int i=0;i<4;i++)
+        for(j=0; j<4; j++)
+            for(i=0; i<4; i++)
                 hold[j][i] = holdtemp[j][i];
-        }
     }
     return 0;
 }
 
 void gturn(void){
     //配列回転の技でセーブしたtemp配列をblock配列に入れ直す→逆回転
-    for(int i=0; i<4; i++){
+    for(int i=0; i<4; i++)
         for(int j=0; j<4; j++)
             block[i][j] = temp[i][j];
-    }
 }
 
 //配列回転の技
 int turnblok(void){
+    int i, j;
     //セーブだよ
-    for(int j=0;j<4;j++){
-        for(int i=0;i<4;i++)
+    for(j=0; j<4; j++)
+        for(i=0; i<4; i++)
             temp[j][i] = block[j][i];
-    }
     //ブロック回転ダ
-    for(int i = 0; i<4; i++){
-        for(int j = 0; j<4; j++)
+    for(i = 0; i<4; i++)
+        for(j = 0; j<4; j++)
             block[i][j] = temp[3-j][i];
-    }
     return 1;
 }
 
 int teityaku(void){
     //地面に接触したものをfield配列に定着させる
-    for(int i=20; i>=0; i--){
-        if(x-1-3<=i && x-1>=i){
-            for(int j=11; j>=0; j--){
-                if(y<=j && y+3>=j){
-                    if(block[3-(x-i)][3-(y-1-j)] == 1){
-                        field[i-1][j+1] = block[3-(x-i)][3-(y-1-j)];
-                    }
-                }
-            }
-        }
-    }
+    for(int i=20; i>=0; i--)
+        for(int j=11; j>=0; j--)
+            if(x-1-3<=i && x-1>=i && y<=j && y+3>=j && block[3-(x-i)][3-(y-1-j)])
+                field[i-1][j+1] = block[3-(x-i)][3-(y-1-j)];
     return 0;
 }
 
@@ -253,10 +236,9 @@ int nextpush(int n){
                            {{0,0,0,0},{0,0,0,0},{0,1,0,0},{1,1,1,0}},
                            {{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}} };
     //詰める
-    for(int j=0;j<4;j++){
-        for(int i=0;i<4;i++)
+    for(int j=0; j<4; j++)
+        for(int i=0; i<4; i++)
             next[j][i][n] = object[randamu][j][i];
-    }
     return randamu;
 
 }
@@ -264,33 +246,32 @@ int nextpush(int n){
 void dainyu(void){
     //一個分押し上げる
     int i, j;
-    for(i=0; i<4; i++){
+    for(i=0; i<4; i++)
         for(j=0; j<4; j++){
             next[i][j][0] = block[i][j] = next[i][j][1];
             next[i][j][1] = next[i][j][2];
             next[i][j][2] = next[i][j][3];
         }
-    }
     nextpush(3);
 }
 
 void nexthyoji(int a){
     //next部分だけの表示を任された関数
-    for(int j=0;j<4;j++){
+    for(int j=0; j<4; j++){
         if(a<4){
-            if(next[a][j][1]==1)
+            if(next[a][j][1])
                 printf("⬜");
             else
                 printf("  ");
         }
         else if(a<8){
-            if(next[a-4][j][2]==1)
+            if(next[a-4][j][2])
                 printf("⬜");
             else
                 printf("  ");
         }
         else if(a<12){
-            if(next[a-8][j][3]==1)
+            if(next[a-8][j][3])
                 printf("⬜");
             else
                 printf("  ");
@@ -303,62 +284,53 @@ int mainhyoji(int a) {
     //field + block
     //main部分だけの表示を任された関数
     //[3-(x-a)]なのはそのままやると上下が反転するから
-    for(int j=0;j<12;j++){
+    for(int j=0; j<12; j++){
         if(j>0 && j<11){
             if(x-a>=0 && j-y-1>=0 && x-a<4 && j-y-1<4){
-                if(field[a][j] == 1 && block[3-(x-a)][j-y-1] == 1){
+                if(field[a][j] && block[3-(x-a)][j-y-1]){
                     printf("??");
                     return 1;
                 }
-                else if((field[a][j] == 1 && block[3-(x-a)][j-y-1] == 0) || (field[a][j] == 0 && block[3-(x-a)][j-y-1] == 1))
+                else if((field[a][j] && !block[3-(x-a)][j-y-1]) || (!field[a][j] && block[3-(x-a)][j-y-1]))
                     printf("⬜");
                 else
                     printf("  ");
             }
             else{
-                if(field[a][j] == 1)
+                if(field[a][j])
                     printf("⬜");
                 else
                     printf("  ");
             }
-            if(x-n>=0 && j-y-1>=0 && x-n<4 && j-y-1<4){
-                if(block[3-(x-n)][j-y-1] == 1)
-                    return 1;
-            }
-    	}
+            if(x-n>=0 && j-y-1>=0 && x-n<4 && j-y-1<4 && block[3-(x-n)][j-y-1])
+                return 1;
+        }
         else{
-            if(x-a>=0 && j-y-1>=0 && x-a<4 && j-y-1<4){
-                if(block[3-(x-a)][j-y-1] == 1)
-                    return 1;
-            }
-            if(x-n>=0 && j-y-1>=0 && x-n<4 && j-y-1<4){
-                if(block[3-(x-n)][j-y-1] == 1)
-                    return 1;
-            }
-
+            if(x-a>=0 && j-y-1>=0 && x-a<4 && j-y-1<4 && block[3-(x-a)][j-y-1])
+                return 1;
+            if(x-n>=0 && j-y-1>=0 && x-n<4 && j-y-1<4 && block[3-(x-n)][j-y-1])
+                return 1;
         }
     }
     return 0;
 }
 
 int holdhyoji(int a){
-    //next部分だけの表示を任された関数
-    for(int j=0;j<4;j++){
+    //hold部分だけの表示を任された関数
+    for(int j=0; j<4; j++)
         if(a<4){
-            if(hold[a][j]==1)
+            if(hold[a][j])
                 printf("⬜");
             else
                 printf("  ");
         }
-    }
     return 0;
 }
 
 int hyoji(void) {
     //枠を表示する中身はほかの関数に任せる
     printf("\033[2J\033[0;0f");
-    int rak;
-    for(int a=0;a<21;a++){
+    for(int a=0; a<21; a++){
         if(a==0)
             printf(" -next-----");
         else if(a==4 || a==8 || a==12)
@@ -375,8 +347,7 @@ int hyoji(void) {
             printf("----------------------");
         else{
             printf("|");
-            rak = mainhyoji(a);
-            if(rak == 1)
+            if(mainhyoji(a))
                 return 1;
             printf("|");
         }
@@ -399,19 +370,17 @@ int hyoji(void) {
 
 void mainclear(void) {
     //field配列を0にリセットしておく
-	int i,j;
-    for(i=0; i<20; i++){
+    int i, j;
+    for(i=0; i<20; i++)
         for(j=0; j<12; j++)
             field[i][j] = 0;
-    }
     /*nextpush関数でnext[][][1]に落ちてくるやつを設定しているので
     それを実際に落ちてくる配列blockと隠し配列next[][][0]にセットする*/
-    for(i=0; i<4; i++){
+    for(i=0; i<4; i++)
         for(j=0; j<4; j++){
             next[i][j][0] = block[i][j] = next[i][j][1];
             hold[i][j] = 0;
         }
-    }
 }
 
 int kbhit(void){
